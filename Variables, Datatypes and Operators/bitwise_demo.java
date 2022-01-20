8bfac38c0cab4166738d2498406fac489381b09f
Siddhant Nivilkar <92569576+Sidd-niv@users.noreply.github.com>
2022-01-20 17:17:14 +0530

    Add files via upload

---
+++ Variables, Datatypes and Operators/bitwise_demo.java
@@ -0,0 +1,19 @@
+
+public class bitwise_demo {
+
+	public static void main(String[] args) {
+		//OR and AND bitwise operator
+		int a = 25, b = 15;// a = 11001, b = 1111
+		int c = a|b; // o/p--> 11111-->31
+		System.out.println("OR operator on a: "+a+ " and b:"+b+" -->"+c);
+		int g= a&b; // o/p-->01001-->9.
+		System.out.println("AND operator on a: "+a+ " and b:"+b+" -->"+g);
+		//right and left shift operator.
+		int e = 25;
+	    double  f = e>>2;
+		System.out.println("Right shift operation on e: "+f);
+		double  j = e<<2;
+		System.out.println("Left shift operation on e: "+j);
+	}
+
+}
