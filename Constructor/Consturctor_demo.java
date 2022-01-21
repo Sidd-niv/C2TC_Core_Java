8a02ee6d82a441b91c978510406e643c32adc672
Siddhant Nivilkar <92569576+Sidd-niv@users.noreply.github.com>
2022-01-21 20:26:38 +0530

    Create Consturctor_demo.java

---
+++ Constructor/Consturctor_demo.java
@@ -0,0 +1,9 @@
+class Consturctor_demo{
+  //As we are not creating a consturctor JVM will call default cinsturctor.
+  
+  public static void main(String[] args){
+    //
+    Consturctor obj = new Consturctor(); // Constructor are used to initialize the objects.
+    // new is a keyword which is used to create an instance of the class.
+  }
+}
