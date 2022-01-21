73ba78499cff87f6bdd8eb5777ba99d8e18f9f8c
Siddhant Nivilkar <92569576+Sidd-niv@users.noreply.github.com>
2022-01-21 20:52:23 +0530

    Update Array_demo.java

--- Array/Array_demo.java
+++ Array/Array_demo.java
@@ -4,8 +4,16 @@ class Array_demo{
   
   public static void main(String[] args){
   // Declaring array with new keyword
+  // As we just declared the size ,then at memory location each element at that index will be initialized with default value i.e.. 0
     int arr_1[] = new int[3]; // When we create array we need to provide the base size
-    // As we just declared the size ,then at memory location each element at that index will be initialized with default value i.e.. 0
+  
+  
+  // Inserting values in array
+    arr1[0] = 2;
+    arr1[1] = 3;
+    arr1[2] = 100;
+    
+    
   // Declaring array directly 
     int arr_2[] = {2 , 3, 5, 7}; 
    
