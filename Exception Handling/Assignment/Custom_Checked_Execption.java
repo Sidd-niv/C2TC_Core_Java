f72da9e373332efb9bdf1117b5c1d2e5fd6c25df
Siddhant Nivilkar <92569576+Sidd-niv@users.noreply.github.com>
2022-01-23 20:21:27 +0530

    Update Custom_Checked_Execption.java

--- Exception Handling/Assignment/Custom_Checked_Execption.java
+++ Exception Handling/Assignment/Custom_Checked_Execption.java
@@ -1,8 +1,14 @@
+// In this code we have created Compiled time i.e Checked Exception with the help of throw and throws keyword.
 package Exception_demo;
+// Importing Scanner class to take user input
 import java.util.Scanner;
 
+
+// Note: To create Cheched exception we need to extend Exception class 
+// Creating user defined exception 
 class Invalid_Input_AgeException extends Exception{
 	
+	// Creating Constructor to initiate the custom message.
 	Invalid_Input_AgeException(String msg){
 		super(msg);
 	}
@@ -11,6 +17,18 @@ class Invalid_Input_AgeException extends Exception{
 
 
 public class Custom_Checked_Execption {
+	
+	// Throws keyword tells the caller of the method that this method can throw exception 
+	// you should use try catch block
+	public void VotingAge(int age) throws Invalid_Input_AgeException {
+		if(age>=18) {
+		  System.out.println("You are eligible for voting"); 
+		}else {
+			// Throw keyword is used to throw Exception directly based on programmer logic
+			throw new Invalid_Input_AgeException("You are not eligible");
+			// We can also pass the message 
+		}
+	}
 
 	public static void main(String[] args) {
 		Custom_Checked_Execption obj = new Custom_Checked_Execption();
@@ -22,12 +40,6 @@ public class Custom_Checked_Execption {
 
 	}
 	
-	public void VotingAge(int age) throws Invalid_Input_AgeException {
-		if(age>=18) {
-		  System.out.println("You are eligible for voting"); 
-		}else {
-			throw new Invalid_Input_AgeException("You are not eligible");
-		}
-	}
+	
 
 }
