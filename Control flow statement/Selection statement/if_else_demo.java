0d5b57c2722418070bc2e032c1769e99e5fc857b
Siddhant Nivilkar <92569576+Sidd-niv@users.noreply.github.com>
2022-01-23 21:22:42 +0530

    Create if_else_demo.java

---
+++ Control flow statement/Selection statement/if_else_demo.java
@@ -0,0 +1,40 @@
+class if_else_demo{
+    
+  public static void main(String[] args){
+  
+    // Simple if else statement
+    int num = 1548;
+    // Curly braces are not mandatory for one statement except for decalarative statement
+    // Result of logical statement which is pass after if must be boolean type.
+    if(num%2==0) 
+      System.out.println("Even number");
+    else
+      System.out.println("Even number");
+    // else is not mandatory 
+    
+    // else if
+    if(num == 345)
+      System.out.println("Yes");
+    else if(num == 1548)
+      System.out.println("Yes");
+    else
+      System.out.println("no");
+    
+    // Nested if else
+    if(num == 1548){
+      System.out.println("Entered inside if block");
+      if(num != 458){
+        System.out.println("Entered inside the nested if block");
+      }
+      else{
+        System.out.println("This is assoicated with inner if");
+      }
+    else{
+       System.out.println("This is assoicated with outer if");
+    }
+      
+    }
+    
+  }
+
+}
