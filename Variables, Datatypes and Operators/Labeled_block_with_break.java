7f3e4643e6fe05daaa0410bcf5f6350e537565cf
Siddhant Nivilkar <92569576+Sidd-niv@users.noreply.github.com>
2022-01-23 20:48:15 +0530

    Create Labeled_block_with_break.java

---
+++ Variables, Datatypes and Operators/Labeled_block_with_break.java
@@ -0,0 +1,23 @@
+// In this code we have shown the use of Break with labeled blocks
+
+class Labeled_block_with_break{
+  
+  public static void main(String[] args){
+    
+    Outer: for(int i=0;i<=5;i++){ // Outer for loop
+     
+      Inner: for(int j=0;j<=i;j++){ // Inner for loop
+        
+        if (j == 4){
+          break Outer;
+        }else if(j == 3){
+          break Inner;
+        }
+        
+      }
+    }
+  
+  
+  }
+
+}
