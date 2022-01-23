9361eb7154024e11876253b3c4e3e76f66d3ddfe
Siddhant Nivilkar <92569576+Sidd-niv@users.noreply.github.com>
2022-01-23 21:40:49 +0530

    Create While_demo.java

---
+++ Control flow statement/Iterative statement/While_demo.java
@@ -0,0 +1,18 @@
+class While_demo{
+  public static void main(String[] args){
+    // Declaring variable
+    int num = 3;
+    
+    // while loop accepts only boolean statement
+    while(num>=2){
+      num--;
+    }
+    
+    // Infinte loop
+    //while(true){
+         //code 
+    // }
+  
+  }
+    
+}
