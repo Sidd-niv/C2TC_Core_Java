6146679afd207760e4cd1f1a6b137fc5f0a96ff5
Siddhant Nivilkar <92569576+Sidd-niv@users.noreply.github.com>
2022-01-23 20:36:02 +0530

    Update Unchecked_exception_demo.java

--- Exception Handling/Assignment/Unchecked_exception_demo.java
+++ Exception Handling/Assignment/Unchecked_exception_demo.java
@@ -1,16 +1,29 @@
+// In this code we have created RuntimeException i.e unchecked exception which checked during runtime 
 package Exception_demo;
-
-import java.util.InputMismatchException;
+// Importing Scanner class to take user input
 import java.util.Scanner;
 
-// Custom Unchecked Exception which can be created by extending RuntimeException class which is the sub-class of Exception class
-
+// To create Custom Unchecked Exception we need to  extend RuntimeException class which is the sub-class of Exception class
+// Extending RuntimeException which is sub-class of Exception class
 class InvalidEntryException extends RuntimeException{
-
+       // Creating the constructor to initiate the custom message
 	InvalidEntryException(String msg){
 		super(msg);
 	}
 }
+
+class Unchecked_demo2{
+	
+	 // Exception inside a method
+	void valid_age(int n) {
+		if(n>18) {
+		System.out.println("You can entry the water ride");
+	        }else {
+		throw new InvalidEntry("You are underage");
+	       }
+       }
+	
+ }
 
 public class Unchecked_exception_demo {
 	
@@ -19,10 +32,10 @@ public class Unchecked_exception_demo {
 		int num = 0;
 		while(true) {
 			try {
-				System.out.println("Enter your age");
+			    System.out.println("Enter your age");
 			    num = user_input.nextInt();
 			}catch (InputMismatchException e  ){
-				
+			       System.out.println(e);
 			}
 			try {
 				if(num > 18) {
@@ -31,30 +44,22 @@ public class Unchecked_exception_demo {
 				}else {
 					throw new InvalidEntryException("You are underage");
 				}
-		}catch (InvalidEntryException e) {
-			e.getMessage();	
-		}
+		        }catch (InvalidEntryException e) {
+			       e.getMessage();	
+		               }
 
-	}System.out.println("Out of the loop\n");
-	// Exception inside a method
-	Unchecked_demo2 obj = new Unchecked_demo2();
-	try {
-	obj.valid_age(num);
-	}catch (InvalidEntryException e) {
-		e.getMessage();	
-	}
-	System.out.println("Execption with method");
-user_input.close();
-}
-}
+	                }System.out.println("Out of the loop\n");
+		
+		
+	              
+	              Unchecked_demo2 obj = new Unchecked_demo2();
+	              try {
+	                   obj.valid_age(num);
+	              }catch (InvalidEntryException e) {
+		           e.getMessage();	
+	              }
+	              System.out.println("Execption with method");
+                      user_input.close();
+                      }
+                }
 
-class Unchecked_demo2{
-	void valid_age(int n) {
-		if(n>18) {
-			System.out.println("You can entry the water ride");
-	}else {
-		throw new InvalidEntry("You are underage");
-	}
-}
-	
-}
