e8e0897fcac367ec07f06c1d215ca8c12fa62e43
Siddhant Nivilkar <92569576+Sidd-niv@users.noreply.github.com>
2022-01-24 10:27:15 +0530

    Update and rename Each_demo.java to For_each_demo.java

---
+++ Control flow statement/Iterative statement/For_each_demo.java
@@ -0,0 +1,18 @@
+// For each loop is used to retrive the from iterative objects. e.g Array
+class For_each_demo{
+  
+  public static void main(String[] args){
+    
+  int arr[] = {2,3, 5, 6}; // Array as been declared with elements
+  int num = 0;
+  
+   // for(each_items (inside variable): target (iterative object) ){
+   //    code;
+   // }
+   
+  // Simple example of for each loop
+  for(int sub_arr: arr)
+    System.out.println("array element at position "+num+"="+sub_arr);
+  
+ }
+}
