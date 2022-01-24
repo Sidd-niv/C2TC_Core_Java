da363922d7c5b124c22e2180ce8768b3969a9a33
Siddhant Nivilkar <92569576+Sidd-niv@users.noreply.github.com>
2022-01-24 10:48:51 +0530

    Create Break_demo.java

---
+++ Control flow statement/Transfer control statement/Break_demo.java
@@ -0,0 +1,39 @@
+// break keyword is used to break the on going loops
+// It is used in switch, loops and labelled blocks.
+
+class Break_demo{
+  
+  public static void main(String[] args){
+    
+    // Without break switch will enter in fall through condition i.e it will execute all case after case no. called.
+    int num = 2;
+    Switch(num)
+    {
+      case 2:
+      //code
+      break;
+      
+      case 4:
+      //code
+      break;
+    }
+    
+    // for loop example
+    for(int i=1; i>=3; i++){
+      if(i == 2)
+        break;
+      System.out.println("Inside the loop");
+    }
+    System.out.println("outside the loop");
+    
+    // while loop example
+    int num2 = 3
+    while(num2 >= 10){
+      if(num2 == 4)
+        break;
+      System.out.println("Inside the loop"); 
+    }
+     System.out.println("outside the loop");
+    
+  }
+}
