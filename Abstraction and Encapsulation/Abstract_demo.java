a4ce332da9c4775e5fa4edb215f3396aa44cbc90
Siddhant Nivilkar <92569576+Sidd-niv@users.noreply.github.com>
2022-01-24 20:37:44 +0530

    Create Abstract_demo.java

---
+++ Abstraction and Encapsulation/Abstract_demo.java
@@ -0,0 +1,51 @@
+// In thois code we will see the declaration of abstract class and method and how to implement them.
+
+// To make a abstract class we use abstract keyword which is access modifier
+abstract class Abstract_demo_class{
+  
+  // Inside abstract we just declare the methods
+  // An abstract can contain nth number method declaration or not even a single one
+  // To declare the abstract method we use abstract keyword
+  // Abstract class can also have an instances method
+  abstract void m1();
+  
+  abstract void m2(int num1, int num2);
+  
+  void m3(){
+    System.out.println("Instances method \n " +
+                       "we can access this method with help of sub-class");
+  }
+}
+
+// Note:  We can't create the object of a abstract class 
+
+
+// To implement the methods of abstract class we need to extend them in another class
+
+class Implement_demo extends Abstract_demo{
+  
+  void m1(){
+    System.out.println("Abstract method m1() body");
+  }
+  
+   void m2(int num1, int num2){
+    System.out.println("Abstract method m2() body");
+  }
+  
+  // If we didn't implement all methods declare in abstract class then we have to also declare the class as abstract
+
+}
+
+
+class Abstract_demo{
+  
+  public static void main(String args[])
+  {
+    // Creating the object of Implement_demo class 
+    Implement_demo obj = new Implement_demo();
+    
+    // Accessing the methods of implemented methods
+    obj.m1();
+    obj.m2();
+  }
+}
