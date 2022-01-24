360d6d2f372c40f95a3d799d8868dab1d28035eb
Siddhant Nivilkar <92569576+Sidd-niv@users.noreply.github.com>
2022-01-24 11:25:46 +0530

    Update Const_init_demo.java

--- Constructor/Const_init_demo.java
+++ Constructor/Const_init_demo.java
@@ -1,4 +1,4 @@
-
+// In this code we will initialize the values inside the class with help of constructor
 public class Const_init_demo {
 	String name;
 	int roll_no; 
@@ -8,7 +8,10 @@ public class Const_init_demo {
 		this.roll_no=roll_no;
 }
 	public static void main(String[] args) {
+		// Initiating the values
 		Const_init_demo obj1=new Const_init_demo("Siddhant", 03);
+		
+		// Print the values 
 		System.out.println("Constructor as initialized the value"+  ": "+ obj1.name +" "+ obj1.roll_no);
 	}
 
