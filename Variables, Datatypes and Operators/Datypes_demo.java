c32ef9a23631c1d650cda3c2fde2889eb991f65c
Siddhant Nivilkar <92569576+Sidd-niv@users.noreply.github.com>
2022-01-24 20:09:33 +0530

    Create Datypes_demo.java

---
+++ Variables, Datatypes and Operators/Datypes_demo.java
@@ -0,0 +1,37 @@
+class Datypes_demo{
+  
+  public static void main(String[] args) {
+    
+    // Signed datatypes.
+    // byte declaration with range.
+       byte num1 = 127; // 1 bit is for + or - sign.
+         
+    // short declaration with range.
+       short num1 = 32,767; // 1 bit is for + or - sign.
+         
+    // int declaration with range.
+       int num1 = 2,147,483,647; // 1 bit is for + or - sign. 
+         
+    // long declaration with range.
+       long num1 = 2,147,483,647L; // 1 bit is for + or - sign. 
+    
+     // float declaration and range same has int. 
+       float num1 = 2.45f; // 1 bit is for + or - sign. 
+     
+     // Double declaration and range same has long.
+       double num1 = 2.45456d; // 1 bit is for + or - sign. 
+         
+    
+         
+         
+     // Unsiged datatypes
+     // Boolean
+        boolean obj = true; // or false.
+    
+     // char
+        char ch = 'A' // Single quotes are used to represent the char datatype in java
+          
+     // 
+        
+  }
+}
