a90cd510fe5475a93f81f7e6256b66cb0523d5c6
Siddhant Nivilkar <92569576+Sidd-niv@users.noreply.github.com>
2022-01-24 10:19:16 +0530

    Create For_loop.java

---
+++ Control flow statement/Iterative statement/For_loop.java
@@ -0,0 +1,24 @@
+public class For_loop {
+	public static void main(String[] args) {
+		// for(initialization;condition;updation){
+    //       code 
+    //     }
+	
+		
+		// Example of for loop
+    // Curly brackets are not mandatory for one statement except for declaretive statement
+		for(int a=0, int b=2; a<b; a++,b--)
+			System.out.println("Hello");
+		
+		
+		for(int a=0; a<5; a++); // Empty Statement
+//Syntax Error due to multiple typecast for same variable
+    // for(int a=0, char='r'; a<4;a++);
+// Invalid syntax for(int a=0, char b="S"; a<=7) we can't declare different datatype.
+// 		for(int a=0; ;a++)
+//     		System.out.println("Hello");  // Infinite loop because of skip condition statement
+// 		System.out.println("reached"); // Hence the loop is infinite it will give compile time error i.e Unreachable code.
+		
+	}
+
+}
