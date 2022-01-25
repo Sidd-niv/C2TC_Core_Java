d16514f530ea716cd795178b73261c4c10eb77ef
Siddhant Nivilkar <92569576+Sidd-niv@users.noreply.github.com>
2022-01-25 15:25:58 +0530

    Update logical_op.java

--- Variables, Datatypes and Operators/logical_op.java
+++ Variables, Datatypes and Operators/logical_op.java
@@ -1,17 +1,28 @@
+package datatypes_variable_op;
+
+// In this code we have shown the how logical operators works.
+// Result of logical operators are boolean type.
+// Before doing logical operators we must understand the work of logical gates, because the same concpt is used in logical operator.
 
 public class logical_op {
 	public static void main(String[] args) {
+		
+		// Declaring int local variables.
 		int num1 = 7,  num2 = 7;
+		
+		
 		// AND logical operator
 		if ((num1==num2) && (num1>6)){
 			System.out.println("True");
 		}
-		// AND logical operator
+		
+		// OR logical operator
 		if ((num1<3) || (num2==5)) {
 			System.out.println("True");
 		}else {
 			System.out.println("False");
 		}
+		
 		// NOT operator
 		boolean human = true;
 		if (!human){
@@ -19,6 +30,7 @@ public class logical_op {
 		}else {
 			System.out.println("I am not a human");
 		}
+		
 		// Ternary operator
 		int x, y;  
 		x = 20;  
