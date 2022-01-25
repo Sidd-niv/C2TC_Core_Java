54f4e07f59bbf60fc899a61468091a74d791d7b6
Siddhant Nivilkar <92569576+Sidd-niv@users.noreply.github.com>
2022-01-25 14:39:55 +0530

    Update Hierarchical_inheritances_demo.java

--- Inheritance/Hierarchical_inheritances_demo.java
+++ Inheritance/Hierarchical_inheritances_demo.java
@@ -1,1 +1,32 @@
-package Inheritance_pack;
+package Inheritances_pack;
+
+// In this code we will see how hierarchical inheritances is implemented in java
+
+// Creating Parent class which will act as a super class
+class Parent{
+  
+  // Declaring Instance variable with String datatype
+  String p_str = "Instance variable of Parent class";
+  
+}
+
+class Child_1 extends Parent{
+  
+  // Declaring Instance variable with String datatype
+  String c1_str = "Instance variable of Child_1 class";
+  
+}
+
+class Child_2 extends Parent{
+  
+  // Declaring Instance variable with String datatype
+  String c2_str = "Instance variable of Child_2 class";
+  
+}
+
+class Child_3 extends Parent{
+  
+  // Declaring Instance variable with String datatype
+  String c1_str = "Instance variable of Child_1 class";
+  
+}
