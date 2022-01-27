4135c2f08b5c12fd95f95f9b80d75813860b1c44
Siddhant Nivilkar <92569576+Sidd-niv@users.noreply.github.com>
2022-01-27 19:39:38 +0530

    Update Const_init_demo.java

--- Constructor/Const_init_demo.java
+++ Constructor/Const_init_demo.java
@@ -3,10 +3,12 @@ public class Const_init_demo {
 	String name;
 	int roll_no; 
 	int r;
+	
+	// This type of constructors are Parameterized consturctor
 	public Const_init_demo(String name, int roll_no) {
 		this.name=name;
 		this.roll_no=roll_no;
-}
+               }
 	public static void main(String[] args) {
 		// Initiating the values
 		Const_init_demo obj1=new Const_init_demo("Siddhant", 03);
