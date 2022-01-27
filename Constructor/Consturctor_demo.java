c5de3f43aca783213f5f50539cb61ccad86f45c8
Siddhant Nivilkar <92569576+Sidd-niv@users.noreply.github.com>
2022-01-27 19:44:14 +0530

    Update Consturctor_demo.java

--- Constructor/Consturctor_demo.java
+++ Constructor/Consturctor_demo.java
@@ -1,6 +1,14 @@
 
 class Consturctor_demo{
   //As we are not creating a consturctor JVM will call default consturctor.
+  //This default constructors are non-argument constructor i.e.. Non Parameterized Constructor.
+  
+  // E.g of default constructor which is created by JVM
+  
+  // Consturctor_demo(){
+  //     super() .. this we call the superclass consturctor.
+  // }
+  
   
   public static void main(String[] args){
     
