9496817d623d289d6eba74e54f977a2c29931851
Siddhant Nivilkar <92569576+Sidd-niv@users.noreply.github.com>
2022-01-27 21:31:12 +0530

    Update do_while_loop.java

--- Control flow statement/Iterative statement/do_while_loop.java
+++ Control flow statement/Iterative statement/do_while_loop.java
@@ -1,3 +1,7 @@
+package loops_pack;
+
+// In this code we will see how to use do while loop.
+
 class do_while_loop{
     public static void main(String[] args){
     // do while statement will run loop by default one time and then it check the while condition
