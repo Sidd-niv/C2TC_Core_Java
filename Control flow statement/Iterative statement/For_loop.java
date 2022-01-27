00b988454737b927d66ea5f40655f782f5ca1503
Siddhant Nivilkar <92569576+Sidd-niv@users.noreply.github.com>
2022-01-27 20:58:36 +0530

    Update For_loop.java

--- Control flow statement/Iterative statement/For_loop.java
+++ Control flow statement/Iterative statement/For_loop.java
@@ -1,23 +1,35 @@
+package loops_pack;
+
+
 public class For_loop {
 	public static void main(String[] args) {
+		
 		// for(initialization;condition;updation){
-    //       code 
-    //     }
+                //       code 
+                //    }
 	
 		
 		// Example of for loop
-    // Curly brackets are not mandatory for one statement except for declaretive statement
+                // Curly brackets are not mandatory for one statement except for declaretive statement
 		for(int a=0, int b=2; a<b; a++,b--)
 			System.out.println("Hello");
 		
+		// Empty Statement
+		for(int a=0; a<5; a++); 
 		
-		for(int a=0; a<5; a++); // Empty Statement
-//Syntax Error due to multiple typecast for same variable
-    // for(int a=0, char='r'; a<4;a++);
-// Invalid syntax for(int a=0, char b="S"; a<=7) we can't declare different datatype.
-// 		for(int a=0; ;a++)
-//     		System.out.println("Hello");  // Infinite loop because of skip condition statement
-// 		System.out.println("reached"); // Hence the loop is infinite it will give compile time error i.e Unreachable code.
+                // Syntax Error due to multiple typecast for same variable
+                // for(int a=0, char='r'; a<4;a++);
+                // Invalid syntax for(int a=0, char b="S"; a<=7) we can't declare different datatype.
+		
+                // for(int a=0; ;a++){
+                //     System.out.println("Hello");  // Infinite loop because of skip condition statement
+	        //    }
+                // System.out.println("reached"); // Hence the loop is infinite it will give compile time error i.e Unreachable code.
+		
+		//Infinte for loop
+		// for(; ;){
+		//      System.out.println("Infinite for loop"); 
+	        //    }
 		
 	}
 
