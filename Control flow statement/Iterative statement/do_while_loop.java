cdfeab89ce0f2fcbdcd74a7cd8b989135ef4cb58
Siddhant Nivilkar <92569576+Sidd-niv@users.noreply.github.com>
2022-01-27 21:41:19 +0530

    Update do_while_loop.java

--- Control flow statement/Iterative statement/do_while_loop.java
+++ Control flow statement/Iterative statement/do_while_loop.java
@@ -7,19 +7,19 @@ class do_while_loop{
     // do while statement will run loop by default one time and then it check the while condition
     int num = 2;
     do{
-      num++;
-      System.out.println(num);
+	    num++;
+            System.out.println(num);
     }while(num>=5);
     
-    int x = 1
-    do{
-        x++;
-		System.out.println(x);
-		if (++x<5)
-			continue;
-		x++;
-		System.out.println(x);	
-		}while(++x<10);
+    int x=0;
+	   do{
+		   x++;
+		   System.out.println(x);
+		   if (++x<5)
+			  continue;
+		   x++;
+		   System.out.println(x);
+	   }while(++x<10);
       
    // Empty do while loop
     do;
